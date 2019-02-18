d6dfef331a2028376aa21f3a18036f8465b42305
Steven Fisher <steven.fisher22@gmail.com>
2019-02-18 13:40:15 -0600

    index.js imports

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        I am the app.js.
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
